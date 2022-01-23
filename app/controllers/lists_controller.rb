d2cefec0b884f7e0ae45bd01853fe470dd9b52a8
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-01-23 16:12:32 +0100

    added style

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,10 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
   def index
     @lists = List.all
   end
 
-  def show
-    @list = List.find(params[:id])
-  end
+  def show; end
 
   def new
     @list = List.new
@@ -20,9 +19,18 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path(@list)
+  end
+
   private
 
   def list_params
     params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: "lists#index"
-  resources :lists, only: [ :index, :show, :new, :create ]
+  resources :lists, except: [ :edit, :update ] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
